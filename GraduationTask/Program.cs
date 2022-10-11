79108306df29d457cd36f40fcff9302a066e33a4
Vit73125 <vit73125@gmail.com>
2022-10-11 19:50:53 +0300

    Reduction 1

---
+++ GraduationTask/Program.cs
@@ -0,0 +1,43 @@
+﻿//===================================================================================
+// # Написать программу, которая из имеющегося массива строк формирует массив из
+// строк, длина которых меньшелибо равна 3 символа.
+//===================================================================================
+
+string[] inputStrings = { "hello", "2", "world", ":-)" };
+PrintStrings(inputStrings);
+PrintStrings(GetFixStrings(inputStrings));
+PrintStrings(inputStrings);
+
+// Выбор строк меньше 3 символов
+string[] GetFixStrings(string[] str)
+{
+    int len = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3)
+        {
+            string buf = str[len];
+            str[len] = str[i];
+            str[i] = buf;
+            len++;
+        }
+    }
+    string[] strOut = new string[len];
+    Array.Copy(str, 0, strOut, 0, len);
+    return strOut;
+}
+
+// Вывод строк
+void PrintStrings(string[] str)
+{
+    Console.Write("[");
+    if (str.Length > 0)
+    {
+        Console.Write("\"" + str[0] + "\"");
+        for (int i = 1; i < str.Length; i++)
+        {
+            Console.Write(", \"" + str[i] + "\"");
+        }
+    }
+    Console.Write("]\n");
+}
